83553245474b23f066be411420c91d6d2e8550ff
piggirlyr <piggirlyr@163.com>
2019-08-31 09:40:50 +0800

    A方法中添加了循环语句

---
+++ A.java
@@ -0,0 +1,14 @@
+package com.yuedabao.git;
+
+public class A {
+    public static void main(String[] args) {
+        fori();
+    }
+
+
+    public  static void fori(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i+"hello,git");
+        }
+    }
+}
